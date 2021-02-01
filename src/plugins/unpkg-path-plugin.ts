4f4406b06634486c2808f192b36e885adc429306
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2021-02-01 22:00:38 +0000

    Added functionality to cache package request in unpkg-path-plugin.ts

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,5 +1,11 @@
 import * as esbuild from 'esbuild-wasm';
 import axios from "axios";
+import localForage from 'localforage';
+
+// Create localForage instance to enable caching npm package requests
+const fileCache = localForage.createInstance({
+  name: 'filecache'
+});
 
 export const unpkgPathPlugin = () => {
   return {
@@ -42,15 +48,27 @@ export const unpkgPathPlugin = () => {
           };
         }
 
+        // Check if we've already fetched the package
+        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
+        // If so, return it from the cache
+        if (cachedResult) {
+          return cachedResult;
+        }
+
+        // If not, let the request occur and cache the returned object
         // Get the data from the unpkg package path, and the request object which has the pathname of where the
         // index.js file is found - for nested packages - e.g. /src/index.js
         const {data, request} = await axios.get(args.path);
         // resolveDir contains the pathname without the /index.js - this is passed on to the next cycle
-        return {
+        const result: esbuild.OnLoadResult = {
           loader: 'jsx',
           contents: data,
           resolveDir: new URL('./', request.responseURL).pathname
         };
+
+        await fileCache.setItem(args.path, result);
+
+        return result;
       });
     },
   };
