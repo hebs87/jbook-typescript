41bf975c50783d7ca8f7b1761550990472e2d92c
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2021-02-01 21:26:49 +0000

    Configured esbuild definitions to resolve console warnings

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,11 @@ const App: FC = (): ReactElement => {
       entryPoints: ['index.js'],
       bundle: true,
       write: false,
-      plugins: [unpkgPathPlugin()]
+      plugins: [unpkgPathPlugin()],
+      define: {
+        'process.env.NODE_ENV': '"production"',
+        global: 'window',
+      },
     });
 
     console.log(result);
--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -36,7 +36,7 @@ export const unpkgPathPlugin = () => {
           return {
             loader: 'jsx',
             contents: `
-              const message = require('nested-test-pkg');
+              const message = require('react');
               console.log(message);
             `,
           };
