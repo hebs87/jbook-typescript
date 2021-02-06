7997a10b76e461eefef84025ce66651b6ffc12d6
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2021-02-06 23:43:49 +0000

    Updated CodeEditor config options

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -4,7 +4,19 @@ import MonacoEditor from '@monaco-editor/react';
 const CodeEditor: FC = (): ReactElement => {
   return (
     <MonacoEditor
+      theme="dark"
+      language="javascript"
       height={500}
+      options={{
+        wordWrap: 'on',
+        minimap: {enabled: false},
+        showUnused: false,
+        folding: false,
+        lineNumbersMinChars: 3,
+        fontSize: 16,
+        scrollBeyondLastLine: false,
+        automaticLayout: true
+      }}
     />
   );
 };
