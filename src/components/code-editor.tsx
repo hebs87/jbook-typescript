a9b42956b9d552a2ce758c8d2a1671a7840a1024
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2021-02-07 00:23:08 +0000

    Introduced ability to format editor content using prettier

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,5 +1,7 @@
-import React, {FC, ReactElement} from 'react';
+import React, {FC, ReactElement, useRef} from 'react';
 import MonacoEditor, {EditorDidMount} from '@monaco-editor/react';
+import prettier from 'prettier';
+import parser from 'prettier/parser-babel';
 
 interface CodeEditorProps {
   initialValue: string;
@@ -7,8 +9,13 @@ interface CodeEditorProps {
 }
 
 const CodeEditor: FC<CodeEditorProps> = ({onChange, initialValue}): ReactElement => {
+  const editorRef = useRef<any>();
+
   // Get value of the editor on change
   const onEditorDidMount: EditorDidMount = (getValue, monacoEditor) => {
+    // Update editorRef value on mount
+    editorRef.current = monacoEditor;
+
     monacoEditor.onDidChangeModelContent(() => {
       onChange(getValue());
     });
@@ -16,24 +23,44 @@ const CodeEditor: FC<CodeEditorProps> = ({onChange, initialValue}): ReactElement
     monacoEditor.getModel()?.updateOptions({tabSize: 2});
   };
 
+  const onFormatClick = () => {
+    // Get current unformatted value from the editor
+    const unformatted = editorRef.current.getModel().getValue();
+
+    // Format the value
+    const formatted = prettier.format(unformatted, {
+      parser: 'babel',
+      plugins: [parser],
+      useTabs: false,
+      semi: true,
+      singleQuote: true
+    });
+
+    // Set the formatted value back in the editor
+    editorRef.current.setValue(formatted);
+  };
+
   return (
-    <MonacoEditor
-      editorDidMount={onEditorDidMount}
-      value={initialValue}
-      theme="dark"
-      language="javascript"
-      height={500}
-      options={{
-        wordWrap: 'on',
-        minimap: {enabled: false},
-        showUnused: false,
-        folding: false,
-        lineNumbersMinChars: 3,
-        fontSize: 16,
-        scrollBeyondLastLine: false,
-        automaticLayout: true
-      }}
-    />
+    <div>
+      <button onClick={onFormatClick}>Format</button>
+      <MonacoEditor
+        editorDidMount={onEditorDidMount}
+        value={initialValue}
+        theme="dark"
+        language="javascript"
+        height={500}
+        options={{
+          wordWrap: 'on',
+          minimap: {enabled: false},
+          showUnused: false,
+          folding: false,
+          lineNumbersMinChars: 3,
+          fontSize: 16,
+          scrollBeyondLastLine: false,
+          automaticLayout: true
+        }}
+      />
+    </div>
   );
 };
 
