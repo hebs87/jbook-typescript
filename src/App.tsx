d5f10d95c33ca30fb035b9913e669b5ecfa617ce
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2021-02-12 22:49:41 +0000

    Created basic TextEditor component and rendered in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React, {FC, ReactElement} from 'react';
 import CodeCell from "./components/CodeCell/code-cell";
+import TextEditor from "./components/TextEditor/text-editor";
 
 const App: FC = (): ReactElement => {
   return (
     <div>
-      <CodeCell/>
+      {/*<CodeCell/>*/}
+      <TextEditor/>
     </div>
   );
 }
--- src/components/TextEditor/text-editor.tsx
+++ src/components/TextEditor/text-editor.tsx
@@ -1,1 +1,11 @@
+import React, {FC, ReactElement} from "react";
+import MDEditor from '@uiw/react-md-editor';
 import './text-editor.styles.scss';
+
+const TextEditor: FC = ():ReactElement => {
+  return(
+    <div>Text Editor</div>
+  );
+};
+
+export default TextEditor;
