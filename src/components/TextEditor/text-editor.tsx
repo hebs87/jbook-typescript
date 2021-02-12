cd252fa2639040466f0ca4394d3c47785e24e18f
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2021-02-12 23:14:19 +0000

    Rendered MDEditor and Markdown components and added logic to conditionally display one or the other

--- src/components/TextEditor/text-editor.tsx
+++ src/components/TextEditor/text-editor.tsx
@@ -1,10 +1,46 @@
-import React, {FC, ReactElement} from "react";
+import React, {FC, ReactElement, Fragment, useState, useEffect, useRef} from "react";
 import MDEditor from '@uiw/react-md-editor';
 import './text-editor.styles.scss';
 
 const TextEditor: FC = ():ReactElement => {
+  const [editing, setEditing] = useState(false);
+  const mdEditorDiv = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const listener = (event: MouseEvent):void => {
+      // Only change editing state when clicking outside the editor div
+      if (mdEditorDiv.current && event.target && mdEditorDiv.current.contains(event.target as Node)) {
+        return;
+      }
+      handleClick(false);
+    };
+
+    document.addEventListener('click', listener, {capture: true});
+
+    return(() => {
+      document.removeEventListener('click', listener, {capture: true});
+    })
+  }, []);
+
+  const handleClick = (value: boolean): void => {
+    setEditing(value);
+  }
+
   return(
-    <div>Text Editor</div>
+    <Fragment>
+      {
+        editing &&
+        <div ref={mdEditorDiv}>
+          <MDEditor/>
+        </div>
+      }
+      {
+        !editing &&
+        <div onClick={() => handleClick(true)}>
+          <MDEditor.Markdown source="# Heading"/>
+        </div>
+      }
+    </Fragment>
   );
 };
 
