10cadab761228083d9535d7246fd466e5ae5da59
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2021-02-13 01:55:15 +0000

    Created useTypedSelector hook and called in CellList component

--- src/components/CellList/cell-list.tsx
+++ src/components/CellList/cell-list.tsx
@@ -1,6 +1,9 @@
 import React, {FC, ReactElement} from "react";
+import {useTypedSelector} from "../../hooks/use-typed-selector";
 
 const CellList: FC = (): ReactElement => {
+  const state = useTypedSelector((state) => state);
+
   return (
     <div>Cell List</div>
   );
