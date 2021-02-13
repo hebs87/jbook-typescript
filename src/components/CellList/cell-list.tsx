7355ea6b971a6fec03a56160b4cd719fcbe5af48
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2021-02-13 01:47:07 +0000

    Created basic CellList and CellListItem components

---
+++ src/components/CellList/cell-list.tsx
@@ -0,0 +1,9 @@
+import React, {FC, ReactElement} from "react";
+
+const CellList: FC = (): ReactElement => {
+  return (
+    <div>Cell List</div>
+  );
+};
+
+export default CellList;
---
+++ src/components/CellListItem/cell-list-item.tsx
@@ -0,0 +1,9 @@
+import React, {FC, ReactElement} from "react";
+
+const CellListItem: FC = (): ReactElement => {
+  return (
+    <div>Cell List Item</div>
+  );
+};
+
+export default CellListItem;
