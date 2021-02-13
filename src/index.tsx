2553c4773876a1ad44423965b0e33fe0125c3592
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2021-02-13 00:34:05 +0000

    Connected App to redux store and added imports to empty files to resolve compiling error

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import App from './App';
+import {Provider} from "react-redux";
+import {store} from "./state";
 import reportWebVitals from './reportWebVitals';
+import 'bulmaswatch/superhero/bulmaswatch.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
