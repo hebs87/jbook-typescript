c01e49ea031d3b483e7e3ef15ebadb03c5aa19d0
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2021-02-13 01:09:48 +0000

    Implemented update and delete cell reducer functionality using immer produce method

--- src/state/reducers/cells-reducer.ts
+++ src/state/reducers/cells-reducer.ts
@@ -1,11 +1,14 @@
 import {ActionType} from "../action-types";
 import {Action} from "../actions";
 import {Cell} from "../cell";
+import produce from "immer";
 
 interface CellsState {
   loading: boolean;
   error: string | null;
+  // order of the cells - array of id strings
   order: string[];
+  // cell objects - key is id string and value is the Cell object
   data: {
     [key: string]: Cell
   }
@@ -18,7 +21,8 @@ const initialState: CellsState = {
   data: {}
 };
 
-const cellsReducer = (state: CellsState = initialState, action: Action): CellsState => {
+// Wrap reducer in immer produce method to enable simplifying state update
+const cellsReducer = produce((state: CellsState = initialState, action: Action) => {
   switch (action.type) {
     case ActionType.MOVE_CELL:
       return state;
@@ -28,24 +32,19 @@ const cellsReducer = (state: CellsState = initialState, action: Action): CellsSt
 
     case ActionType.UPDATE_CELL:
       const {id, content} = action.payload;
-      // The cell key in the data object is the id - we get the id key, spread in the current props and update content
-      return {
-        ...state,
-        data: {
-          ...state.data,
-          [id]: {
-            ...state.data[id],
-            content
-          }
-        }
-      };
+      // Update the content of the cell at the matching id key
+      state.data[id].content = content;
+      return;
 
     case ActionType.DELETE_CELL:
-      return state;
+      // Delete cell at the matching id key and remove id from order array
+      delete state.data[action.payload];
+      state.order = state.order.filter(id => id !== action.payload);
+      return;
 
     default:
       return state;
   }
-};
+});
 
 export default cellsReducer;
