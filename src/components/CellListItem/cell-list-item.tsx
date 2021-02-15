9c45e6fb971d7cf1b69b58106bff0c46e1ee5ae1
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2021-02-15 23:17:30 +0000

    Refactored value/setValue logic in TextEditor to use redux state instead

--- src/components/CellListItem/cell-list-item.tsx
+++ src/components/CellListItem/cell-list-item.tsx
@@ -8,7 +8,7 @@ interface CellListItemProps {
 }
 
 const CellListItem: FC<CellListItemProps> = ({cell}): ReactElement => {
-  const child: JSX.Element = cell.type === 'code' ? <CodeCell cell={cell}/> : <TextEditor/>;
+  const child: JSX.Element = cell.type === 'code' ? <CodeCell cell={cell}/> : <TextEditor cell={cell}/>;
 
   return (
     <div>{child}</div>
--- src/components/TextEditor/text-editor.tsx
+++ src/components/TextEditor/text-editor.tsx
@@ -1,11 +1,17 @@
 import React, {FC, ReactElement, Fragment, useState, useEffect, useRef} from "react";
 import MDEditor from '@uiw/react-md-editor';
+import {Cell} from "../../state";
+import {useActions} from "../../hooks/use-actions";
 import './text-editor.styles.scss';
 
-const TextEditor: FC = ():ReactElement => {
+interface TextEditorProps {
+  cell: Cell;
+}
+
+const TextEditor: FC<TextEditorProps> = ({cell}):ReactElement => {
   const [editing, setEditing] = useState(false);
-  const [value, setValue] = useState('# Header');
   const mdEditorDiv = useRef<HTMLDivElement | null>(null);
+  const {updateCell} = useActions();
 
   useEffect(() => {
     const listener = (event: MouseEvent):void => {
@@ -27,18 +33,14 @@ const TextEditor: FC = ():ReactElement => {
     setEditing(value);
   }
 
-  const handleEditorChange = (value: string): void => {
-    setValue(value);
-  }
-
   return(
     <Fragment>
       {
         editing &&
         <div className="text-editor" ref={mdEditorDiv}>
           <MDEditor
-            value={value}
-            onChange={(value) => handleEditorChange(value || '')}
+            value={cell.content}
+            onChange={(value) => updateCell(cell.id, value || '')}
           />
         </div>
       }
@@ -47,7 +49,7 @@ const TextEditor: FC = ():ReactElement => {
         <div className="text-editor card" onClick={() => handleClick(true)}>
           <div className="card-content">
             <MDEditor.Markdown
-              source={value}
+              source={cell.content || 'Click to edit...'}
             />
           </div>
         </div>
