51eeedbc81ae0eb0c677368397ac16bc7d4e5b7e
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2021-02-18 23:02:29 +0000

    Resolved style bugs with code editor window

--- src/components/CellList/cell-list.tsx
+++ src/components/CellList/cell-list.tsx
@@ -2,6 +2,7 @@ import React, {FC, ReactElement, Fragment} from "react";
 import CellListItem from "../CellListItem/cell-list-item";
 import AddCell from "../AddCell/add-cell";
 import {useTypedSelector} from "../../hooks/use-typed-selector";
+import './cell-list.styles.scss';
 
 const CellList: FC = (): ReactElement => {
   const cells = useTypedSelector(({cells: {data, order}}) => (
@@ -16,7 +17,7 @@ const CellList: FC = (): ReactElement => {
   ));
 
   return (
-    <div>
+    <div className="cell-list">
       <AddCell forceVisible={cells.length === 0} previousCellId={null}/>
       {renderedCells}
     </div>
