6e3aa69412571486bbf923a102aa4605ef72d5ce
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2021-02-20 02:48:08 +0000

    Added post logic to createCellsRouter

--- packages/local-api/src/routes/cells.ts
+++ packages/local-api/src/routes/cells.ts
@@ -1,7 +1,16 @@
 import express from "express";
+import fs from "fs/promises";
+import path from "path";
+
+interface Cell {
+  id: string;
+  content: string;
+  type: 'text' | 'code';
+}
 
 export const createCellsRouter = (filename: string, dir: string) => {
   const router = express.Router();
+  const fullPath = path.join(dir, filename);
 
   router.get('/cells', async (req, res) => {
     // Make sure cell storage file exists
@@ -12,13 +21,13 @@ export const createCellsRouter = (filename: string, dir: string) => {
   });
 
   router.post('/cells', async (req, res) => {
-    // Make sure cell storage file exists
-
-    // If no file, create it
-
     // Take list of cells from req obj and serialize them
+    const {cells}: {cells: Cell[]} = req.body;
 
     // Write the cells into the file
+    await fs.writeFile(fullPath, JSON.stringify(cells), 'utf-8');
+
+    res.send({status: 'success'});
   });
 
   return router;
