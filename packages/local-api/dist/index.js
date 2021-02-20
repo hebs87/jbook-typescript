c8af4ce56c0297249320d9a6679204bc13c8a728
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2021-02-20 02:07:20 +0000

    Created proxy from local-api to react app

--- packages/local-api/dist/index.js
+++ packages/local-api/dist/index.js
@@ -5,9 +5,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.serve = void 0;
 var express_1 = __importDefault(require("express"));
+var http_proxy_middleware_1 = require("http-proxy-middleware");
 // Args - port to run on, filename of the save/fetch file, dir to save the file into
 var serve = function (port, filename, dir) {
     var app = express_1.default();
+    // Create proxy to port that React app is running on
+    app.use(http_proxy_middleware_1.createProxyMiddleware({
+        target: 'http://localhost:3000',
+        ws: true,
+        logLevel: 'silent',
+    }));
     // Resolve promise if successful, or reject and put into error state on error
     return new Promise(function (resolve, reject) {
         app.listen(port, resolve).on('error', reject);
--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -1,11 +1,19 @@
-import express from 'express';
+import express from "express";
+import {createProxyMiddleware} from "http-proxy-middleware";
 
 // Args - port to run on, filename of the save/fetch file, dir to save the file into
 export const serve = (port: number, filename: string, dir: string) => {
   const app = express();
 
+  // Create proxy to port that React app is running on
+  app.use(createProxyMiddleware({
+    target: 'http://localhost:3000',
+    ws: true,
+    logLevel: 'silent',
+  }));
+
   // Resolve promise if successful, or reject and put into error state on error
-  return new Promise<void>((resolve, reject) =>{
+  return new Promise<void>((resolve, reject) => {
     app.listen(port, resolve).on('error', reject);
   });
 };
