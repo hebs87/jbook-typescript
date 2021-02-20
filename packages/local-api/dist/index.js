1ad752c5148c1e26a939157be2c1d00e11d3db3a
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2021-02-20 02:23:16 +0000

    Added functionality to serve react app build files in local-api

--- packages/local-api/dist/index.js
+++ packages/local-api/dist/index.js
@@ -5,16 +5,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.serve = void 0;
 var express_1 = __importDefault(require("express"));
-var http_proxy_middleware_1 = require("http-proxy-middleware");
+var path_1 = __importDefault(require("path"));
 // Args - port to run on, filename of the save/fetch file, dir to save the file into
 var serve = function (port, filename, dir) {
     var app = express_1.default();
+    // Serve React app build files - get absolute path to node_modules dir in local machine
+    var packagePath = require.resolve('local-client/build/index.html');
+    app.use(express_1.default.static(path_1.default.dirname(packagePath)));
     // Create proxy to port that React app is running on
-    app.use(http_proxy_middleware_1.createProxyMiddleware({
-        target: 'http://localhost:3000',
-        ws: true,
-        logLevel: 'silent',
-    }));
+    // app.use(createProxyMiddleware({
+    //   target: 'http://localhost:3000',
+    //   ws: true,
+    //   logLevel: 'silent',
+    // }));
     // Resolve promise if successful, or reject and put into error state on error
     return new Promise(function (resolve, reject) {
         app.listen(port, resolve).on('error', reject);
